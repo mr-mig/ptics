1927b8f8719bbd98e77b788e0a8f7c52f4857c0c
Alex Migutsky <alexey.migutsky@gmail.com>
2024-10-16 09:58:27 +0000

    Add svelte-query to plugin

--- plugin/src/App.svelte
+++ plugin/src/App.svelte
@@ -1,12 +1,22 @@
 <script type="ts">
-    import Router from 'svelte-spa-router';
-    import routes from './routes';
-    import { replace } from 'svelte-spa-router';
+    import Router from "svelte-spa-router";
+    import routes from "./routes";
+    import {
+        QueryClient,
+        QueryClientProvider,
+    } from "@sveltestack/svelte-query";
+    import { replace } from "svelte-spa-router";
+    import User from "./components/User.svelte";
 
     const conditionsFailed = () => {
-        console.log('conditions failed');
-        replace('/join')
-    }
+        console.log("conditions failed");
+        replace("/join");
+    };
+
+    const queryClient = new QueryClient();
 </script>
 
-<Router {routes} on:conditionsFailed={conditionsFailed}/>
+<QueryClientProvider client={queryClient}>
+    <User/>
+    <Router {routes} on:conditionsFailed={conditionsFailed} />
+</QueryClientProvider>
